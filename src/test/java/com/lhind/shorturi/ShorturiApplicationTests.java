66bded3d51c37ef2d3a39c204acc7ce8b66c605f
Lisen Saka <lisensaka@gmail.com>
2025-05-29 22:24:57 +0200

    initializing project with github

---
+++ src/test/java/com/lhind/shorturi/ShorturiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhind.shorturi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
