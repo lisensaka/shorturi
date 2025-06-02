38c51608b8a814c57effcda24bbc1a8686d372d2
Lisen Saka <lisensaka@gmail.com>
2025-06-02 17:33:15 +0200

    pushing new SwaggerConfig version

--- src/main/java/com/lhind/shorturi/config/openai/SwaggerConfig.java
+++ src/main/java/com/lhind/shorturi/config/openai/SwaggerConfig.java
@@ -1,8 +1,6 @@
 package com.lhind.shorturi.config.openai;
 
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -12,20 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@OpenAPIDefinition(
-//        info = @Info(
-//                title = "URL Shortener API",
-//                version = "1.0",
-//                description = "JWT secured API for shortening URLs"),
-//        security = @SecurityRequirement(
-//                name = "bearerAuth")
-//)
-//@SecurityScheme(
-//        name = "bearerAuth",
-//        type = SecuritySchemeType.HTTP,
-//        scheme = "bearer",
-//        bearerFormat = "JWT"
-//)
 public class SwaggerConfig {
 
     @Bean
